9cb6d59f1d5a6e48c83f47c3ae009c7f24be3e9b
ed <eddranca@gmail.com>
2023-10-11 21:18:06 +0300

    Add the attribute value serialization to the default ObjectMapper

--- src/main/kotlin/com/freecourses/config/JacksonConfig.kt
+++ src/main/kotlin/com/freecourses/config/JacksonConfig.kt
@@ -1,24 +1,21 @@
 package com.freecourses.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.Module
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.freecourses.persistence.utils.jackson.StringAttributeValueDeserializer
 import com.freecourses.persistence.utils.jackson.StringAttributeValueSerializer
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 
 @Configuration
 open class JacksonConfig {
-    @Bean("attributeValueMapper")
-    open fun objectMapper(@Autowired attributeValueSerializer: StringAttributeValueSerializer, @Autowired attributeValueDeserializer: StringAttributeValueDeserializer): ObjectMapper {
-        val objectMapper = ObjectMapper()
+    @Bean
+    open fun objectMapper(attributeValueSerializer: StringAttributeValueSerializer, attributeValueDeserializer: StringAttributeValueDeserializer): Module {
         val module = SimpleModule()
         module.addSerializer(AttributeValue::class.java, attributeValueSerializer)
         module.addDeserializer(AttributeValue::class.java, attributeValueDeserializer)
-        objectMapper.registerModule(module)
-        return objectMapper
+        return module
     }
 
     @Bean
